4d5f8cea555f11df50a767a94867b8606ff5b5f6
Makhliyo Ablakulova <m.ablakulova@itransition.com>
2022-09-29 17:19:18 +0500

    Time: 109 ms (77.36%), Space: 37.4 MB (87.95%)

---
+++ 1672-richest-customer-wealth/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetcodeLocalClasses
+{
+    public class Solution 
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int richest_wealth = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                int temp_wealth = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                    temp_wealth += accounts[i][j];
+
+                richest_wealth = richest_wealth > temp_wealth ? richest_wealth : temp_wealth;
+            }
+
+            return richest_wealth;
+        }
+    }
+}
