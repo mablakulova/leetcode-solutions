d568695b841bfb80f0e8795dba31c833ecf3cb47
Makhliyo Ablakulova <m.ablakulova@itransition.com>
2022-10-02 12:23:16 +0500

    Time: 255 ms (22.33%), Space: 46.9 MB (85.46%)

---
+++ 1480-running-sum-of-1d-array/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetcodeLocalClasses
+{
+    public class Solution 
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for(int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
+    }
+}
