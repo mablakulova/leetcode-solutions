4ef9c48065df97fc49829a4d0eb959e6b391ac51
mablakulova <abmakhliyo@gmail.com>
2022-12-24 02:53:26 +0500

    Runtime: 254 ms (93.4%), Memory: 55.9 MB (22.22%)

---
+++ 344-reverse-string/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode;
+
+public class Solution {
+    public void ReverseString(char[] s)
+    {
+        int start = 0, end = s.Length - 1;
+
+        while (start < end)
+        {
+            var temp = s[start];
+            s[start++] = s[end];
+            s[end--] = temp;
+        }
+    }
+}
+
