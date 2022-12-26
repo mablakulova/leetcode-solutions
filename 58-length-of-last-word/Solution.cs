1661364deb2e1474e0144c8975f55634c37b955d
mablakulova <abmakhliyo@gmail.com>
2022-12-26 10:49:10 +0500

    Runtime: 60 ms (95.31%), Memory: 36.4 MB (24.8%)

---
+++ 58-length-of-last-word/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode;
+
+public class Solution {
+    public int LengthOfLastWord(string s)
+    {
+        string trimmedString = s.Trim();
+        int count = 0;
+
+        for (int i = trimmedString.Length - 1; i >= 0; i--)
+        {
+            if (trimmedString[i] == ' ')
+                return count;
+            else
+                count++;
+        }
+
+        return count;
+    }
+}
+
