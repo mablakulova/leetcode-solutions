ce2be0920e0305c3979f68413f141b84beeae42e
mablakulova <abmakhliyo@gmail.com>
2023-01-09 07:13:21 +0500

    Runtime: 101 ms (91.78%), Memory: 43.8 MB (27.1%)

---
+++ 268-missing-number/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode;
+
+public class Solution {
+    public int MissingNumber(int[] nums)
+    {
+        int len = nums.Length;
+        int sum = 0, nSum = 0;
+        
+        foreach (int num in nums)
+        {
+            sum += num;
+        }
+
+        nSum = (1 + len) * len / 2;
+
+        return nSum - sum;
+    }
+}
+
