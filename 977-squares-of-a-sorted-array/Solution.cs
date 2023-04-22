234f9cc9922baaf0324815c9795458f7d07aef1b
mablakulova <abmakhliyo@gmail.com>
2023-04-22 17:05:58 +0500

    Runtime: 164 ms (81.18%), Memory: 59.2 MB (90.53%)

---
+++ 977-squares-of-a-sorted-array/Solution.cs
@@ -0,0 +1,15 @@
+namespace LeetCode;
+
+public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        Array.Sort(nums);
+        return nums;
+    }
+}
