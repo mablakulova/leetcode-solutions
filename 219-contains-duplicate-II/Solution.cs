b5c13a1dc11e892aee75b9285c538aa0e6c43e15
mablakulova <abmakhliyo@gmail.com>
2023-07-15 10:25:54 +0500

    Runtime: 192 ms (99.85%), Memory: 59.36 MB (33.97%)

---
+++ 219-contains-duplicate-II/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode;
+
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var myDictionary = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (myDictionary.ContainsKey(nums[i]))
+            {
+                if (i - myDictionary[nums[i]] <= k)
+                    return true;
+                else
+                    myDictionary[nums[i]] = i;
+            }
+            else
+                myDictionary.Add(nums[i], i);
+        }
+
+        return false;
+    }
+}
