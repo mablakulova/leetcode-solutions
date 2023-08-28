75404269d95e229145ce4446e2d72ae05d248198
mablakulova <abmakhliyo@gmail.com>
2023-08-29 00:02:38 +0500

    Runtime: 99 ms (69.68%), Memory: 43.84 MB (55.81%)

---
+++ 136-single-number/Solution.cs
@@ -0,0 +1,12 @@
+namespace LeetCode;
+
+public int SingleNumber(int[] nums) {
+    int ans = 0;
+    
+    for(int i = 0; i < nums.Length; i++)
+    {
+        ans ^= nums[i];
+    }
+
+    return ans;
+}
