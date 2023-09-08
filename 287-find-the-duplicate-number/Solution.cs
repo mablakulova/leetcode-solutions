3927426c3e487c3701cefcdb6fdb47c090979bd8
mablakulova <abmakhliyo@gmail.com>
2023-09-08 15:31:32 +0500

    Runtime: 193 ms (39.24%), Memory: 51 MB (72.74%)

---
+++ 287-find-the-duplicate-number/Solution.cs
@@ -0,0 +1,15 @@
+namespace Leetcode;
+
+public class Solution {
+    public int FindDuplicate(int[] nums) 
+    {
+        var set = new HashSet<int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(set.Contains(nums[i])) return nums[i];
+            else set.Add(nums[i]);
+        }
+
+        return 0;
+    }
+}
