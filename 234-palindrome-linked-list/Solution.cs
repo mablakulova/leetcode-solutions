029f95154e3a142e41fc7d7b45b79f88b4152251
mablakulova <abmakhliyo@gmail.com>
2023-09-10 00:25:05 +0500

    Runtime: 215 ms (70.05%), Memory: 64.71 MB (24.94%)

---
+++ 234-palindrome-linked-list/Solution.cs
@@ -0,0 +1,49 @@
+namespace LeetCode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        ListNode reverseHalf = ReverseList(MiddleNode(head));
+        while(reverseHalf != null)
+        {
+            if(reverseHalf.val != head.val) return false;
+            reverseHalf = reverseHalf.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    private ListNode MiddleNode(ListNode head) 
+    {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.next;
+    }
+
+    private ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        while(head != null) 
+        {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
