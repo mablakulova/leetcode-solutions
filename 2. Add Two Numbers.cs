91672fff6c0617e7034c215897abc8d7063e47ba
mablakulova <abmakhliyo@gmail.com>
2023-09-12 23:27:14 +0500

    Runtime: 64 ms (99.93%), Memory: 49.60 MB (34.57%)

---
+++ 2. Add Two Numbers.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        var dummyHead = new ListNode(0);
+        var curr = dummyHead;
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry > 0)
+        {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        return dummyHead.next;
+    }
+}
